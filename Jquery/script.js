c07b85714c77f94ece08e5005e3801c9523884d4
Seyyit atım <seyyit.atim@ogr.sakarya.edu.tr>
2022-02-06 12:58:46 +0300

    add jquery note

---
+++ Jquery/script.js
@@ -0,0 +1,17 @@
+// Selectors
+// console.log($("#liste"));
+// console.log($("h3"));
+// console.log($(".item"));
+
+// Styling
+// $("#header").css("color", "blue");
+// $("a").css("text-decoration", "none");
+
+// var styles = {
+//     color: "yellow",
+//     fontSize: "20px",
+//     border: "1px solid red"
+// }
+
+// $("ul li").css(styles);
+
